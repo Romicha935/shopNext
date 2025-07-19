d1eda177be7b12f6e0a78a4c4d0c383e7ef4d6ee
romicha-parvin <romichaparvin35@gmail.com>
2025-07-19 18:56:44 +0600

    best seling added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,20 +5,41 @@ import HomeCarousel from '@/components/shared/home/home-carousel'
 import { data } from '@/lib/data'
 import CardItem from '@/components/shared/home/home-card'
 import ProductCard from '@/components/shared/product/product-card'
+import BestSelling from '@/components/shared/product/best-selingProducts'
+import RelatedProducts from '@/components/shared/product/related-products'
 
 
 export default function HomePage ()  {
   return (
-          <div>
-            <HomeCarousel items={data.carousels}/>
+          <div className='bg-gray-5'>
+            <HomeCarousel  items={data.carousels}/>
             <CardItem cards={data.featureSections}/>
             {/* <ProductCard product={data.todaysDeals}/> */}
-            <div className='bg-gray-100'>
+            <div className='bg-white py-3'>
             <h2 className='text-2xl font-bold px-8'>Today's Deals</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2  p-4">
          
         {data.todaysDeals.map((product) => (
           <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+          </div>
+            <div className='bg-white'>
+            <h2 className='text-2xl font-bold px-8'>Best Sellings</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2  p-4">
+         
+        {data.bestSellingProducts.slice(0,6).map((product) => (
+          <BestSelling key={product.name} product={product} />
+        ))}
+      </div>
+          </div>
+
+           <div className='bg-white'>
+            <h2 className='text-2xl font-bold px-8'>Related Products</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2  p-4">
+         
+        {data.relatedProducts.slice(0,6).map((product) => (
+          <RelatedProducts key={product.name} product={product} />
         ))}
       </div>
           </div>
--- src/components/shared/product/related-products.tsx
+++ src/components/shared/product/related-products.tsx
@@ -9,8 +9,10 @@ import Rating from './rating'
 import ProductPrice from './product-price'
 import { IProduct } from '@/types'
 // import { IProduct } from '@/types'
+// import { IProduct } from '@/types'
 
-const ProductCard = ({
+
+const RelatedProducts = ({
   product,
   hideBorder = false,
   hideDetails = false,
@@ -31,12 +33,9 @@ const ProductCard = ({
 
   return (
     
-
-        
-      <Link
-      href={`/product/${product.slug}`}
+ <Link href={`/product/${product.slug}`}
       className={cn(
-        'group block  hover:shadow-lg transition overflow-hidden bg-gray-100',
+        'group block  hover:shadow-lg transition overflow-hidden bg-white',
         {
           'border-none': hideBorder
         }
@@ -62,43 +61,10 @@ const ProductCard = ({
         )}
       </div>
 
-      {/* Details */}
-      {!hideDetails && (
-        <div className="p-4 space-y-2 text-center">
-          <p className="text-sm font-bold text-gray-600">{product.brand}</p>
-
-          <h2 className="text-md font-semibold line-clamp-2 min-h-[48px]">
-            {product.name}
-          </h2>
-
-          <p className="text-sm text-gray-500 line-clamp-2 min-h-[40px]">
-            {product.description}
-          </p>
-
-          {/* Rating */}
-          <div className="flex justify-center items-center gap-2">
-            <Rating rating={product.avgRating} />
-            <span className="text-xs text-gray-500">({product.numReviews})</span>
-          </div>
-
-          {/* Price + Discount */}
-          <div className="flex flex-col items-center gap-1">
-            <ProductPrice
-              price={product.price}
-              listPrice={product.listPrice}
-              isDeal={product.tags.includes('todays-deal')}
-            />
-            {percentage > 0 && (
-              <span className="text-xl rounded-lg bg-red-500 text-white px-2 py-1 font-medium">
-                {percentage}% OFF
-              </span>
-            )}
-          </div>
-        </div>
-      )}
+     
     </Link>
  
   )
 }
 
-export default ProductCard
+export default RelatedProducts
