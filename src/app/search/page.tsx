618a7d3955ac40e9b69e96c73b6caf2cf4bfb592
romicha-parvin <romichaparvin35@gmail.com>
2025-08-19 08:10:52 +0600

    updated

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,20 +1,20 @@
-
 import clientPromise from "@/lib/db"
 import { IProduct } from "@/types"
 import ProductCard from "@/components/product-card"
 
-interface Props {
+export const dynamic = "force-dynamic" 
+export default async function SearchPage({
+  searchParams,
+}: {
   searchParams?: { tag?: string }
-}
-
-export const dynamic = "force-dynamic" // live fetch
-
-export default async function SearchPage({ searchParams }: Props) {
+}) {
   const tag = searchParams?.tag
 
   const client = await clientPromise
   const db = client.db(process.env.MONGODB_DB ?? "nextShop")
-  const products = await db.collection<IProduct>("todays-deal")
+
+  const products = await db
+    .collection<IProduct>("todays-deal")
     .find(tag ? { tags: tag } : {})
     .toArray()
 
@@ -30,15 +30,18 @@ export default async function SearchPage({ searchParams }: Props) {
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
             {products.map((p) => (
-              <ProductCard key={p._id} product={{
-                _id: p._id,
-                slug: p.slug,
-                title: p.name,
-                price: p.price,
-                dealPrice: p.listPrice < p.price ? p.listPrice : undefined,
-                image: p.images[0],
-                category: p.brand
-              }} />
+              <ProductCard
+                key={p._id.toString()}
+                product={{
+                  _id: p._id,
+                  slug: p.slug,
+                  title: p.name,
+                  price: p.price,
+                  dealPrice: p.listPrice < p.price ? p.listPrice : undefined,
+                  image: p.images[0],
+                  category: p.brand,
+                }}
+              />
             ))}
           </div>
         )}
