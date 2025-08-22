44782dada09f988c9c58159f4388e1f0d69dc50a
romicha-parvin <romichaparvin35@gmail.com>
2025-08-22 08:20:21 +0600

    error solve

--- src/provider/Provider.tsx
+++ src/provider/Provider.tsx
@@ -56,7 +56,7 @@ const AuthProvider = ({children}: { children: React.ReactNode }) => {
       setIsLoading(false)
     })
     return () => unsubscribe()
-  }, [])
+  }, [auth])
 
     const authInfo = {
         user,
